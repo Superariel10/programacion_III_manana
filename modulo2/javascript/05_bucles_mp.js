e5926123f32713841402db3ed22f1bd6bd189e56
IDIC <alumnos@alumnosuio.ute.edu.ec>
2025-10-23 09:57:53 -0500

    clase 10-23-2025

--- modulo2/javascript/05_bucles_mp.js
+++ modulo2/javascript/05_bucles_mp.js
@@ -12,3 +12,46 @@ let nombre="ariel";
 for(let i=0; i <=nombre.length;i++){
     console.log(nombre[i]);
 }
+for(let i=1; i <=5;i++){
+    console.log("cuadrado de ", i," es ",i**2);
+}
+console.log("Bucle While");
+let i=1
+while(i<=5){
+    console.log("indice ", i)
+    i++;
+}
+let miArreglo=[2,0,1,4,3];
+let indice=1
+while(indice<=miArreglo.length){
+    console.log("valor ",indice, "es", miArreglo[indice]);
+    indice++;
+}
+let numero=1
+while(numero<=8){
+    if(numero%2==0)
+    console.log("Numero ",numero, "es par");
+    numero++;
+}
+
+let x = 4
+do {
+    console.log("x es: ", x)
+    x--;
+} while (x!=0)
+
+let mult = 1
+while(mult<=10) {
+    console.log(mult, "*4=", mult*4);
+    mult++; 
+} 
+let arreglo=[0,1,2,3,4,];
+let n = 0
+let n2= 0
+while(n<=arreglo.lenght) {
+    if (arreglo[n]>n2){
+        n2=arreglo[n]
+    }
+    n++;
+} 
+console.log(mayor);
