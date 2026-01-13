664b2c61b095d0c08142e4e10c20f4ce9f3c58d3
IDIC <alumnos@alumnosuio.ute.edu.ec>
2026-01-13 10:00:32 -0500

    Clase: 13/01/2026

---
+++ modulo4/blog-app/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+import BasicButtons from './lab/BasicButtons'
+import BasicTable from './lab/BasicTable'
+import ColorSwitches from './lab/ColorSwitches'
+import TestMUI from './lab/TestMUI'
+
+function App() {
+
+  return (
+    <>
+      <BasicButtons/>
+      <ColorSwitches/>
+      <BasicTable/>
+      <TestMUI/>
+    </>
+  )
+}
+
+export default App
